9ae97f091f93be49e5bfed0a9747bee319f842cd
Jonathan Leung <jonathan.leung@mail.mcgill.ca>
2015-08-18 17:56:18 -0700

    <Infinite Sidescroller>

---
+++ Assets/SampleAssets/2D/Scripts/Platformer2DUserControl.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnitySampleAssets.CrossPlatformInput;
+
+namespace UnitySampleAssets._2D
+{
+
+    [RequireComponent(typeof (PlatformerCharacter2D))]
+    public class Platformer2DUserControl : MonoBehaviour
+    {
+        private PlatformerCharacter2D character;
+        private bool jump = false;
+
+        private void Awake()
+        {
+            character = GetComponent<PlatformerCharacter2D>();
+        }
+
+        private void Update()
+        {
+            // Read the jump input in Update so button presses aren't missed.
+			if (CrossPlatformInputManager.GetButtonDown ("Jump"))
+				jump = true;
+        }
+
+        private void FixedUpdate()
+        {
+            // Read the inputs.
+            //bool crouch = Input.GetKey(KeyCode.LeftControl);
+            //float h = CrossPlatformInputManager.GetAxis("Horizontal");
+
+            // Pass all parameters to the character control script.
+            character.Move(1, false, jump);
+
+			// Reset the jump input once it has been used
+            jump = false;
+        }
+    }
+}
