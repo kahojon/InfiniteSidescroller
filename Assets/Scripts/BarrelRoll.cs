8c149e694d4e20551d1e3adba9990be1342ba144
Jonathan Leung <jonathan.leung@mail.mcgill.ca>
2015-08-20 22:52:49 -0700

    <Barrel Roll>

---
+++ Assets/Scripts/BarrelRoll.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BarrelRoll : MonoBehaviour {
+	public float speed = 300;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (Vector3.forward * speed * Time.deltaTime, Space.World);
+		Vector2 finalTransform = new Vector2 (-2f, 0f);
+		transform.Translate (finalTransform, Space.World);
+	}	
+}
