800a5f9e6b1be0577dbe33f3a0c1738f3a86b8bb
Jonathan Leung <jonathan.leung@mail.mcgill.ca>
2015-08-20 23:01:03 -0700

    <BarrelSpawn>

---
+++ Assets/Scripts/BarrelSpawn.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BarrelSpawn : MonoBehaviour {
+	
+	public GameObject[] obj;
+
+	void Start(){
+		Spawn ();
+	}
+	
+	void Spawn()
+	{
+		Instantiate (obj[0],new Vector3(transform.position.x,transform.position.y),Quaternion.identity);
+		Invoke (Spawn (), 2);
+	}
+}
